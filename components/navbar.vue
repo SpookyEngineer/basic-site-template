dbef7d318f768aefb37e61e444c3d925f36ee8bb
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-08-31 10:40:06 -0300

    Now using navbarLinkDropdown inside navbar component

--- components/navbar.vue
+++ components/navbar.vue
@@ -2,36 +2,13 @@
   <div class="border-y">
     <nav class="flex justify-center align-middle bg-white py-2">
       <nuxt-link class="mx-2" to="/">Home</nuxt-link>
-
-      <UDropdown
-        :items="items"
-        mode="hover"
-        :popper="{ placement: 'bottom-start' }"
-      >
-        <!-- The below is the dropdown label -->
-        <span v-on:click="routingFunction('/about')"
-          >About Us <Icon name="i-heroicons-chevron-down-20-solid"
-        /></span>
-
-        <template #item="{ item }">
-          <span v-on:click="routingFunction(`/about#${item.subRoute}`)">{{
-            item.label
-          }}</span>
-        </template>
-      </UDropdown>
+      <NavbarLinkDropdown :name="name" :items="items" />
     </nav>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from "nuxt/app";
-
-const router = useRouter();
-
-function routingFunction(route: string) {
-  router.push(route);
-}
-
+const name = "About Us";
 const items = [
   [
     {
