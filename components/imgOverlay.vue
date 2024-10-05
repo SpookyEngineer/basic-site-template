c083af1af310077e9cae1a5fae5b7baa15626d84
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-10-05 12:43:31 -0300

    feat: commencing lar de idosos

---
+++ components/imgOverlay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative w-full overflow-hidden">
+    <slot name="img" class="block object-cover" />
+    <div class="overlay">
+      <slot name="text" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent overlay */
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,39 @@
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-      >Learn more about Nuxt Routing</a
+    <ImgOverlay>
+      <template #img>
+        <img
+          class="w-full block max-h-96 object-cover"
+          src="/images/lar_idosos.webp"
+          alt="Sample Image"
+        />
+      </template>
+      <template #text>
+        <div>
+          <h1 class="text-5xl">LAR DE IDOSOS</h1>
+          <button
+            class="bg-aconchego-green py-2 px-6 rounded-lg mt-4 font-bold"
+          >
+            Ligue-nos
+          </button>
+        </div>
+      </template>
+    </ImgOverlay>
+
+    <div
+      class="flex flex-col items-center justify-center text-center py-16 px-32"
     >
+      <div class="text-3xl">
+        <h2 class="text-aconchego-red mb-2">VEJA AQUI AS NOSSAS INSTALAÇÕES</h2>
+        <h2 class="">CLÍNICA DE REPOUSO O ACONCHEGO</h2>
+      </div>
+      <p class="font-roboto mt-2">
+        O nosso Lar de Idosos, em Gondomar, tem como principais objetivos
+        combater o isolamento social e prestar o <br />
+        devido acompanhamento aos utentes das zonas do Porto, Gaia, Valongo,
+        Maia e Matosinhos.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -12,6 +41,6 @@
 const route = useRoute();
 
 useHead({
-  title: "Home Page",
+  title: "Clinica de Repouso",
 });
 </script>
