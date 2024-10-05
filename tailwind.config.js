a1df941e1cd4d2a71036b912152ff1f94e37691e
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-10-05 12:41:16 -0300

    feat: added roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     extend: {
       colors: {
         "aconchego-green": "#559a28",
+        "aconchego-red": "#E03F3D",
+      },
+      fontFamily: {
+        roboto: ['"Roboto"', "sans-serif"],
       },
     },
   },
