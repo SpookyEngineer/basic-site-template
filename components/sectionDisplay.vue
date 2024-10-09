b220c847d7b277367154f43ccdb2b82b26d4acdf
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-10-09 08:25:04 -0300

    feat: created sectionDisplay component

---
+++ components/sectionDisplay.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <ImgOverlay>
+      <template #img>
+        <img
+          class="w-full block max-h-96 object-cover"
+          :src="`/images/${sectionData.img}`"
+          :alt="sectionData.imgTitle"
+          :title="sectionData.imgTitle"
+        />
+      </template>
+      <template #text>
+        <h2 v-if="sectionData.title" class="text-2xl md:text-4xl font-ubuntu">
+          {{ sectionData.title }}
+        </h2>
+        <p v-if="sectionData.subTitle">{{ sectionData.subTitle }}</p>
+        <button
+          v-if="sectionData.buttonContent"
+          class="bg-aconchego-green py-2 px-6 rounded-lg mt-4 font-bold font-roboto"
+        >
+          {{ sectionData.buttonContent }}
+        </button>
+      </template>
+    </ImgOverlay>
+    <GreyscaleTextImages
+      v-if="sectionData.greyscaleSection"
+      :imagesData="sectionData.greyscaleSection?.gridImages"
+    >
+      <template #text>
+        <p
+          class="text-center"
+          v-html="breakLine(sectionData.greyscaleSection?.text)"
+        />
+      </template>
+    </GreyscaleTextImages>
+    <div class="mx-8 my-4 md:hidden">
+      <CarouselMobile
+        :carouselData="sectionData.greyscaleSection?.gridImages"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface GridImagesItem {
+  img: string;
+  imgTitle: string;
+}
+
+interface SectionDataItem {
+  img: string;
+  imgTitle: string;
+  title: string;
+  subTitle?: string;
+  buttonContent?: string;
+  greyscaleSection?: {
+    text: string;
+    gridImages: GridImagesItem[];
+  };
+}
+
+type SectionData = SectionDataItem;
+
+interface Props {
+  sectionData: SectionData;
+}
+
+const props = defineProps<Props>();
+
+// Makes each sentence be on a new line
+function breakLine(text: string) {
+  return text.replace(/([.?!])\s/g, "$1<br>");
+}
+</script>
