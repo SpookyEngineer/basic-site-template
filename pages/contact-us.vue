8bc4f6bd6535a07d10f074febe43b5ca8aa71abc
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-10-12 19:33:13 -0300

    feat: contact page added

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,85 +1,48 @@
 <template>
   <div>
-    <div class="contact-container">
-      <h1>Contact Us</h1>
-      <form class="contact-form">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div class="form-group">
-          <label for="message">Message</label>
-          <textarea id="message" name="message" rows="4" required></textarea>
-        </div>
-        <button type="submit">Send Message</button>
-      </form>
+    <!-- Speak With Us Section -->
+    <SectionDisplay :section-data="speakWithUsSection" />
+
+    <!-- Contact Us Section -->
+    <div
+      class="flex flex-col justify-center items-center text-4xl font-ubuntu-condensed mb-16"
+    >
+      <h2 class="text-aconchego-red mt-8">CONTACTE-NOS</h2>
+      <h3 class="mb-8">CLÍNICA DE REPOUSO O ACONCHEGO</h3>
+      <img src="/images/team.webp" alt="Team" title="Time" />
     </div>
+
+    <!-- Location Section -->
+    <SectionDisplay :section-data="locationSection" />
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48059.676867672!2d-8.663153091728478!3d41.16224682872561!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2465abc4e153c1%3A0xa648d95640b114bc!2sPorto%2C%20Portugal!5e0!3m2!1sen!2sbr!4v1728771447227!5m2!1sen!2sbr"
+      height="450"
+      style="border: 0; width: 100vw"
+      allowfullscreen="false"
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    />
+
+    <FormSection id="contact-form" />
   </div>
 </template>
 
 <script setup lang="ts">
+const speakWithUsSection = {
+  img: "lar_idosos.webp",
+  imgTitle: "Fale Conosco",
+  title: "FALE CONOSCO",
+  buttonContent: "Ligue-nos",
+};
+
+const locationSection = {
+  img: "lar_idosos.webp",
+  imgTitle: "Localização",
+  title: "LOCALIZAÇÃO",
+  subTitle: "Visite as nossas instalações! Marque já a sua visita!",
+};
+
 useHead({
-  title: "Contact Us",
+  title: "Contate-nos",
 });
 </script>
-
-<style scoped>
-.contact-container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  background-color: #f9f9f9;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-h1 {
-  text-align: center;
-  color: #333;
-}
-
-.contact-form {
-  display: flex;
-  flex-direction: column;
-}
-
-.form-group {
-  margin-bottom: 15px;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-  font-weight: bold;
-  color: #555;
-}
-
-input[type="text"],
-input[type="email"],
-textarea {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-}
-
-button {
-  padding: 10px 15px;
-  border: none;
-  border-radius: 4px;
-  background-color: #007bff;
-  color: #fff;
-  font-size: 16px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-}
-
-button:hover {
-  background-color: #0056b3;
-}
-</style>
